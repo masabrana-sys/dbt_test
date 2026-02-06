bf15fe7481119f01eb3e7e3a66f6099533b30a69
Masab Rana <masab.rana@dbtlabs.com>
2026-02-06 16:22:18 +0000

    new

--- models/staging/sgt_orders__jaffle_shop_raw.sql
+++
@@ -1,9 +0,0 @@
-    select
-        id as order_id,
-        customer as customer_id,
-        ordered_at, 
-        store_id,
-        order_status
-
-    from {{ source('JAFFLE_SHOP_RAW', 'RAW_ORDERS') }}
-
