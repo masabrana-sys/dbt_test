db7f933f594efe46f9e584d05527646bdcae288a
Masab Rana <masab.rana@dbtlabs.com>
2026-03-04 12:48:50 +0000

    added seeds

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -10,6 +10,10 @@ orders as (
 
 ),
 
+employees as (
+    select * from {{ ref('employees') }}
+),
+
 customer_orders as (
 
     select
@@ -33,12 +37,13 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce (customer_orders.number_of_orders, 0) 
-        as number_of_orders
+        coalesce (customer_orders.number_of_orders, 0)  as number_of_orders,
+        e.employee_id
 
     from customers
 
     left join customer_orders using (customer_id)
+    left join employees e on e.customer_id = customers.customer_id
 
 )
 
